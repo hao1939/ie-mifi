75931b329c2463f8efbede942ccbaf952c3b44b2
Hao Yuan <hao1939@gmail.com>
2015-04-27 14:46:15 +0000

    fix test

--- spec/request_spec.rb
+++ spec/request_spec.rb
@@ -79,9 +79,10 @@ describe MyApp do
 
   it 'test_auth_request' do
     request_body = "1\x04\xFF\xFF\xFF\x01'\x00\x88\x00\x81\"\x10\xABe%\xC5\x90Q\xD3\"\xEE&\x10\xBD\xE4)\xD1\x9D\x10\xD9\x01\xDB\x87A\x13rL\xFE1\xAE\xCA\xCD\x03K\x8F\b\x10\x82>>F:\xD7\xD4\x02\x01\x02\x02\x03\x04\x01\x05".b
-    post '/auth', request_body, "CONTENT_TYPE"=>"application/octet-stream"
-
-    assert_equal 200, last_response.status
+    Mifi::CardReader.stub(:auth, 'ok') do
+      post '/auth', request_body, "CONTENT_TYPE"=>"application/octet-stream"
+      assert_equal 200, last_response.status
+    end
   end
 
   it 'test_beat_request' do
